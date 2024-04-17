dbc17d181482b6cf70afe8f8a7f91d85ff13285b
codemarque <segun@workwithmarque.com>
2024-04-17 16:24:38 +0100

    update-login

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [EventController::class, 'index'])->name('home');
+Route::get('/', [EventController::class, 'index']);
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('welcome');
 })->name('login');
