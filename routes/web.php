b687babcee7a40580bba831e8de2f77570f03e23
codemarque <segun@workwithmarque.com>
2024-04-17 16:28:51 +0100

    update-login

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,8 @@ Route::get('/', [EventController::class, 'index']);
 Route::get('/login', function () {
     return view('welcome');
 })->name('login');
+
+
+Route::get('/test', function () {
+    return view('welcome');
+})->name('test');
