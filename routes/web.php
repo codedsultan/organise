50b20700d5fdc1082e426796c7bcd922fbcbb1f2
codemarque <segun@workwithmarque.com>
2024-04-17 15:08:18 +0100

    add-composer

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [EventController::class, 'index']);
+Route::get('/', [EventController::class, 'index'])->name('home');
 
 // Route::get('/', function () {
 //     return view('welcome');
